1e5cdfb47d615cbd7df333e209da681bcf81a584
TakuKitamura <tikutaku0128@icloud.com>
2019-11-09 14:32:53 +0900

    sepalate func

--- server.py
+++ server.py
@@ -1,45 +1,10 @@
 from bottle import run, route, response, template
 import time
-import shutil
 import os
-import pyshark
-import struct
-import atexit
 import subprocess
 import json
-# from datetime import datetime
-packts_dir_path = './packets/'
-def get_tcp_payloads():
-    id = 1
-    capture = pyshark.LiveCapture(interface='lo0', display_filter='mqtt')
-    capture.sniff(timeout=0)
-    for packet in capture.sniff_continuously():
-        # print(1)
-        payload_list = str(packet.tcp.payload).split(':')
-        # id = str(datetime.now().timestamp())
-        file_name = packts_dir_path + str(id) + '_packet.bin'
-        with open(file_name, 'wb') as f:
-            for x in payload_list:
-                f.write(struct.pack("B", int(x, 16)))
-        executed_file_path = './mqttPacketParser.out'
-        command = [executed_file_path, file_name]
-        # print(command)
-        res = subprocess.check_output(command).decode('utf-8').split('\n')
-        json_dict = {'id': id, 'mqtt_packet': list(payload_list)}
-        for el in res[:len(res)-1]:
-            # print()
-            splite_equal = el.rsplit('=', 1)
-            # print(splite_equal)
-            if len(splite_equal) != 2:
-                print('err_data:', res)
-                print('split equal error.')
-            else:
-                dic_name = splite_equal[0]
-                dic_value = splite_equal[1]
-                json_dict[dic_name] = dic_value
-        json_string = json.dumps(json_dict, ensure_ascii=False)
-        yield '{}\n\n'.format(json_string)
-        id += 1
+
+PACKET_DIR_PATH = './packets/'
 
 @route('/')
 def index():
@@ -49,16 +14,17 @@ def index():
 def sse():
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Content_Type']  = 'text/event-stream'
+    mqtt_packet_json_path = PACKET_DIR_PATH + 'mqtt_packet.json'
+    if (os.path.isfile(mqtt_packet_json_path)):
+        with open(mqtt_packet_json_path) as f:
+            yield 'data:{}\n\nretry:{}\n\n'.format(f.read(), 2000)
+    else:
+        yield 'data:{}\n\nretry:{}\n\n'.format('mqtt_packet_json is not found.', 2000)
     # while (True):
     # id = 1
-    tcp_payloads = get_tcp_payloads()
-    # print(1)
-    for payload in tcp_payloads:
-        # print(1)
-        yield 'data:{}\n\n'.format(payload)
-def when_exit():
-    shutil.rmtree(packts_dir_path)
-
-atexit.register(when_exit)
-os.mkdir(packts_dir_path)
+    # tcp_payloads = get_tcp_payloads()
+    # # print(1)
+    # for payload in tcp_payloads:
+    #     # print(1)
+    #     yield 'data:{}\n\n'.format(payload)
 run(host='localhost', port=3000, debug=False, reloader=False)
---
+++ tcpPayloadCapture.py
@@ -0,0 +1,52 @@
+import pyshark
+import struct
+import subprocess
+import json
+import os
+from datetime import datetime
+
+PACKET_DIR_PATH = './packets/'
+
+def append_json_to_file(data, path_file) -> bool:
+    with open(path_file, 'ab+') as f:
+        f.seek(0,2)
+        if f.tell() == 0 :
+            f.write(json.dumps([data], ensure_ascii=False, separators=(',', ':')).encode('utf-8'))
+        else :
+            f.seek(-1,2)
+            f.truncate()
+            f.write(','.encode('utf-8'))
+            f.write(json.dumps(data, ensure_ascii=False, separators=(',', ':')).encode('utf-8'))
+            f.write(']'.encode('utf-8'))
+
+def get_tcp_payloads():
+    capture = pyshark.LiveCapture(interface='lo0', display_filter='mqtt')
+    for packet in capture.sniff_continuously():
+        payload_list = str(packet.tcp.payload).split(':')
+        timestamp = datetime.now().timestamp()
+        file_name = PACKET_DIR_PATH + str(timestamp) + '_packet.bin'
+        with open(file_name, 'wb') as f:
+            for x in payload_list:
+                f.write(struct.pack("B", int(x, 16)))
+        executed_file_path = './mqttPacketParser.out'
+        command = [executed_file_path, file_name]
+        res = subprocess.check_output(command).decode('utf-8').split('\n')
+        json_dict = {'timestamp': timestamp, 'mqtt_packet': list(payload_list)}
+        for el in res[:len(res)-1]:
+            splite_equal = el.rsplit('=', 1)
+            if len(splite_equal) != 2:
+                print('err_data:', res)
+                print('split equal error.')
+            else:
+                dic_name = splite_equal[0]
+                dic_value = splite_equal[1]
+                json_dict[dic_name] = dic_value
+        # json_string = json.dumps(json_dict, ensure_ascii=False)
+        mqtt_packet_json_path = PACKET_DIR_PATH + 'mqtt_packet.json'
+
+        append_json_to_file(json_dict, mqtt_packet_json_path)
+
+if __name__ == "__main__":
+    if not os.path.exists(PACKET_DIR_PATH):
+        os.makedirs(PACKET_DIR_PATH)
+    get_tcp_payloads()
